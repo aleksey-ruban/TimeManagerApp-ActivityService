36881c9a375edc24850c4bf017b6be8944b5bbf3
aleksey-ruban <alekseyruban555@gmail.com>
2025-11-20 00:33:13 +0300

    Init commit

---
+++ src/test/java/com/alekseyruban/timemanagerapp/activity_service/ActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekseyruban.timemanagerapp.activity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
