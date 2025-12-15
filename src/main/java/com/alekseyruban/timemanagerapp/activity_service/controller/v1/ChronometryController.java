c72190e2d360a3be812bf07dd8e2aacfdaae0bd3
aleksey-ruban <alekseyruban555@gmail.com>
2025-12-15 19:38:53 +0300

    Chronometry online service + controller

---
+++ src/main/java/com/alekseyruban/timemanagerapp/activity_service/controller/v1/ChronometryController.java
@@ -0,0 +1,56 @@
+package com.alekseyruban.timemanagerapp.activity_service.controller.v1;
+
+import com.alekseyruban.timemanagerapp.activity_service.DTO.chronometry.CancelChronometryDto;
+import com.alekseyruban.timemanagerapp.activity_service.DTO.chronometry.ChronometryDto;
+import com.alekseyruban.timemanagerapp.activity_service.DTO.chronometry.CreateChronometryDto;
+import com.alekseyruban.timemanagerapp.activity_service.DTO.chronometry.FinishChronometryDto;
+import com.alekseyruban.timemanagerapp.activity_service.DTO.response.ApiResponse;
+import com.alekseyruban.timemanagerapp.activity_service.entity.ChronometrySnapshot;
+import com.alekseyruban.timemanagerapp.activity_service.service.ChronometrySnapshotOnlineService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/activities/chronometry")
+public class ChronometryController {
+
+    private final ChronometrySnapshotOnlineService chronometrySnapshotOnlineService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ChronometryDto>> createChronometry(
+            @RequestHeader("X-User-Id") Long userId,
+            @Valid @RequestBody CreateChronometryDto dto
+    ) {
+        ChronometrySnapshot chronometrySnapshot = chronometrySnapshotOnlineService.createChronometry(userId, dto);
+        ChronometryDto chronometryDto = ChronometryDto.fromChronometry(chronometrySnapshot);
+
+        ApiResponse<ChronometryDto> response = new ApiResponse<>("Chronometry created", chronometryDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/finish")
+    public ResponseEntity<ApiResponse<ChronometryDto>> finishChronometry(
+            @RequestHeader("X-User-Id") Long userId,
+            @Valid @RequestBody FinishChronometryDto dto
+    ) {
+        ChronometryDto chronometryDto = chronometrySnapshotOnlineService.finishChronometry(userId, dto);
+
+        ApiResponse<ChronometryDto> response = new ApiResponse<>("Chronometry finished", chronometryDto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Void> cancelChronometry(
+            @RequestHeader("X-User-Id") Long userId,
+            @Valid @RequestBody CancelChronometryDto dto
+    ) {
+        chronometrySnapshotOnlineService.cancelChronometry(userId, dto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
---
+++ src/main/java/com/alekseyruban/timemanagerapp/activity_service/service/ChronometrySnapshotOnlineService.java
@@ -0,0 +1,259 @@
+package com.alekseyruban.timemanagerapp.activity_service.service;
+
+import com.alekseyruban.timemanagerapp.activity_service.DTO.chronometry.*;
+import com.alekseyruban.timemanagerapp.activity_service.entity.*;
+import com.alekseyruban.timemanagerapp.activity_service.entity.snapshot.ActivityRecordSnapshot;
+import com.alekseyruban.timemanagerapp.activity_service.entity.snapshot.ActivitySnapshot;
+import com.alekseyruban.timemanagerapp.activity_service.entity.snapshot.ActivityVariationSnapshot;
+import com.alekseyruban.timemanagerapp.activity_service.entity.snapshot.CategorySnapshot;
+import com.alekseyruban.timemanagerapp.activity_service.exception.ExceptionFactory;
+import com.alekseyruban.timemanagerapp.activity_service.respository.*;
+import com.alekseyruban.timemanagerapp.activity_service.utils.Locale;
+import com.alekseyruban.timemanagerapp.activity_service.utils.RetryOptimisticLock;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.*;
+import java.util.*;
+
+@Service
+@AllArgsConstructor
+public class ChronometrySnapshotOnlineService {
+
+    private final ChronometrySnapshotRepository chronometrySnapshotRepository;
+    private final ActivityRecordSnapshotRepository activityRecordSnapshotRepository;
+    private final CategorySnapshotRepository categorySnapshotRepository;
+    private final ActivitySnapshotRepository activitySnapshotRepository;
+    private final ActivityRecordRepository activityRecordRepository;
+    private final UserRepository userRepository;
+    private final ExceptionFactory exceptionFactory;
+
+    private static final Set<String> SUPPORTED_LOCALES = Set.of("en", "ru");
+
+    @RetryOptimisticLock
+    @Transactional
+    public ChronometrySnapshot createChronometry(Long userDomainId, CreateChronometryDto dto) {
+        User user = userRepository.findByDomainId(userDomainId)
+                .orElseThrow(exceptionFactory::userNotFountException);
+
+        ZoneId zone = ZoneId.of(dto.getTimeZone());
+        LocalDate todayInZone = LocalDate.now(zone);
+
+        List<ChronometrySnapshot> chronometrySnapshotList = chronometrySnapshotRepository.findByUser_DomainIdAndFinishedFalse(userDomainId);
+        if (!chronometrySnapshotList.isEmpty()) {
+            throw exceptionFactory.chronometryAlreadyExistsException();
+        }
+//        for (ChronometrySnapshot c : chronometrySnapshotList) {
+//            if (c.getEndDate() == null || c.getEndDate().isAfter(todayInZone)) {
+//                return c;
+//            }
+//        }
+
+        LocalDate startDate = resolveLocalStartDate(dto.getCreateTime(), dto.getTimeZone());
+        LocalDate endDate = startDate.plusDays(6);
+
+        ChronometrySnapshot chronometry = ChronometrySnapshot.builder()
+                .startDate(startDate)
+                .endDate(endDate)
+                .timeZone(zone.getId())
+                .user(user)
+                .build();
+
+        return chronometrySnapshotRepository.save(chronometry);
+    }
+
+    @RetryOptimisticLock
+    @Transactional
+    public void cancelChronometry(Long userDomainId, CancelChronometryDto dto) {
+        User user = userRepository.findByDomainId(userDomainId)
+                .orElseThrow(exceptionFactory::userNotFountException);
+
+        ChronometrySnapshot chronometry = chronometrySnapshotRepository.findById(dto.getId())
+                .orElseThrow(exceptionFactory::chronometryNotFoundException);
+
+        if (chronometry.getFinished()) {
+            throw exceptionFactory.chronometryAlreadyExistsException();
+        }
+
+        chronometrySnapshotRepository.delete(chronometry);
+    }
+
+    @RetryOptimisticLock
+    @Transactional
+    public ChronometryDto finishChronometry(Long userDomainId, FinishChronometryDto dto) {
+        User user = userRepository.findByDomainId(userDomainId)
+                .orElseThrow(exceptionFactory::userNotFountException);
+
+        if (!Objects.equals(user.getSnapshotVersion(), dto.getSnapshotVersion())) {
+            throw exceptionFactory.oldVersion();
+        }
+
+        ChronometrySnapshot chronometry = chronometrySnapshotRepository.findById(dto.getId())
+                .orElseThrow(exceptionFactory::chronometryNotFoundException);
+
+        LocalDate endDate = chronometry.getEndDate();
+        if (endDate == null) {
+            endDate = chronometry.getStartDate().plusDays(6);
+        }
+
+        if (!availableForFinish(dto.getFinishTime(), dto.getTimeZone(), endDate)) {
+            throw exceptionFactory.tooEarlyFinishChronometry();
+        }
+
+        String zoneId = chronometry.getTimeZone();
+        Instant fromInstant = chronometry.getStartDate()
+                .atStartOfDay(ZoneId.of(zoneId))
+                .toInstant();
+        Instant toInstant = endDate
+                .plusDays(1)
+                .atStartOfDay(ZoneId.of(zoneId))
+                .toInstant();
+
+        List<ActivityRecord> activityRecords = activityRecordRepository.findOverlappingByUserDomainId(
+                userDomainId,
+                fromInstant,
+                toInstant
+        );
+
+        String lang = SUPPORTED_LOCALES.contains(dto.getLocal()) ? dto.getLocal() : "en";
+
+        List<ActivityRecordSnapshot> activityRecordsSnapshots = new ArrayList<>();
+        Map<Long, ActivitySnapshot> activitiesSnapshotCache = new HashMap<>();
+        Map<Long, Map<Long, ActivityVariationSnapshot>> activityVariationSnapshotCache = new HashMap<>();
+        Map<Long, CategorySnapshot> categoriesSnapshotCache = new HashMap<>();
+
+        for (ActivityRecord ar : activityRecords) {
+            Activity activity = ar.getActivity();
+            ActivityVariation variation = ar.getVariation();
+            Category category = activity.getCategory();
+
+            CategorySnapshot categorySnapshot = categoriesSnapshotCache.computeIfAbsent(
+                    category.getId(),
+                    id -> {
+                        CategorySnapshot cs = new CategorySnapshot();
+                        String name = resolveBaseName(category, Locale.fromCode(lang));
+                        cs.setBaseName(name);
+                        return cs;
+                    }
+            );
+
+            for (ActivityVariation av : activity.getVariations()) {
+                if (Objects.equals(ar.getVariation().getId(), av.getId())) {
+                    activityVariationSnapshotCache.computeIfAbsent(
+                            activity.getId(),
+                            id -> new HashMap<>()
+                    );
+                    ActivityVariationSnapshot vs = ActivityVariationSnapshot.builder()
+                            .value(av.getValue())
+                            .build();
+                    activityVariationSnapshotCache.get(activity.getId()).computeIfAbsent(av.getId(), id -> vs);
+                }
+            }
+
+            ActivitySnapshot activitySnapshot = activitiesSnapshotCache.computeIfAbsent(
+                    activity.getId(),
+                    id -> ActivitySnapshot.from(activity, categorySnapshot)
+            );
+            activitySnapshot.getVariations().add(activityVariationSnapshotCache.get(activity.getId()).get(ar.getVariation().getId()));
+            activityVariationSnapshotCache.get(activity.getId()).get(ar.getVariation().getId()).setActivity(activitySnapshot);
+
+            ActivityRecordSnapshot recordSnapshot = ActivityRecordSnapshot.from(
+                    ar,
+                    activitySnapshot,
+                    activityVariationSnapshotCache.get(activity.getId()).get(variation.getId()),
+                    chronometry
+            );
+            clampRecord(recordSnapshot, fromInstant, toInstant);
+
+            activityRecordsSnapshots.add(recordSnapshot);
+        }
+
+        categorySnapshotRepository.saveAll(categoriesSnapshotCache.values());
+        activitySnapshotRepository.saveAll(activitiesSnapshotCache.values());
+        activityRecordSnapshotRepository.saveAll(activityRecordsSnapshots);
+
+        chronometry.setFinished(true);
+        chronometry.getActivityRecords().addAll(activityRecordsSnapshots);
+        chronometry = chronometrySnapshotRepository.save(chronometry);
+
+        ChronometryDto chronometryDto = ChronometryDto.fromChronometry(chronometry);
+        chronometryDto.setCategorySnapshotList(
+                categoriesSnapshotCache.values().stream()
+                        .map(CategorySnapshotDto::fromCategorySnapshot)
+                        .toList()
+        );
+        chronometryDto.setActivitySnapshotList(
+                activitiesSnapshotCache.values().stream()
+                        .map(ActivitySnapshotDto::fromActivitySnapshot)
+                        .toList()
+        );
+        chronometryDto.setActivityVariationSnapshotList(
+                activityVariationSnapshotCache.values().stream()
+                        .flatMap(innerMap -> innerMap.values().stream())
+                        .map(ActivityVariationSnapshotDto::fromActivityVariation)
+                        .toList()
+        );
+        chronometryDto.setActivityRecordSnapshotList(
+                activityRecordsSnapshots.stream()
+                        .map(ActivityRecordSnapshotDto::fromActivityRecordSnapshot)
+                        .toList()
+        );
+
+        return chronometryDto;
+    }
+
+
+    private LocalDate resolveLocalStartDate(Instant nowUtc, String userTimeZone) {
+        ZoneId zone = ZoneId.of(userTimeZone);
+
+        ZonedDateTime userLocal = nowUtc.atZone(zone);
+
+        LocalDate today = userLocal.toLocalDate();
+
+        LocalTime threshold = LocalTime.of(9, 0);
+
+        if (userLocal.toLocalTime().isBefore(threshold)) {
+            return today;
+        } else {
+            return today.plusDays(1);
+        }
+    }
+
+    private boolean availableForFinish(Instant nowUtc, String userTimeZone, LocalDate chronometryEnd) {
+        ZoneId zone = ZoneId.of(userTimeZone);
+        ZonedDateTime userNow = nowUtc.atZone(zone);
+
+        LocalDate localDateNow = userNow.toLocalDate();
+
+        if (localDateNow.isAfter(chronometryEnd)) {
+            return true;
+        }
+
+        if (localDateNow.isBefore(chronometryEnd)) {
+            return false;
+        }
+
+        LocalTime cutoff = LocalTime.of(21, 0);
+        return userNow.toLocalTime().isAfter(cutoff);
+    }
+
+    private String resolveBaseName(Category category, Locale locale) {
+        for (CategoryLocale l : category.getLocales()) {
+            if (Objects.equals(l.getLocale(), locale.getCode())) {
+                return l.getName();
+            }
+        }
+        return category.getBaseName();
+    }
+
+    private void clampRecord(ActivityRecordSnapshot record, Instant start, Instant end) {
+        if (record.getStartedAt().isBefore(start)) {
+            record.setStartedAt(start);
+        }
+
+        if (record.getEndedAt().isAfter(end)) {
+            record.setEndedAt(end);
+        }
+    }
+}
